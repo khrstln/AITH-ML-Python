cdf3cebce5e1548eb4ad6ebef376ddf6ebb22999
khrstln <Khrstln@yandex.ru>
2025-05-16 12:47:35 +0300

    refactor: изменил структуру репозитория

--- core/use_cases/user_use_cases.py
+++
@@ -1,7 +0,0 @@
-class UserUseCases:
-    def __init__(self, user_repository):
-        self.user_repository = user_repository
-
-    def register_user(self, name: str, email: str):
-        user = User(id=None, name=name, email=email)
-        return self.user_repository.save(user)
