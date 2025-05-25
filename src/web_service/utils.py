887e689a6d78beeca6889529133b040a3f94805b
khrstln <Khrstln@yandex.ru>
2025-05-25 12:35:06 +0300

    chore: фикс импортов

--- src/web_service/utils.py
+++ src/web_service/utils.py
@@ -3,8 +3,7 @@ from typing import Any
 
 import pika
 from dotenv import load_dotenv
-from sqlalchemy.ext.asyncio import (AsyncSession, async_sessionmaker,
-                                    create_async_engine)
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 from sqlalchemy.orm import DeclarativeBase
 
 load_dotenv()
